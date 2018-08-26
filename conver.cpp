8563eed779e278f0c9982a1c8047a34f7ff60efb
Manza21 <supakornmanza21@gmail.com>
2018-08-26 18:38:40 +0700

    Update conver.cpp

--- conver.cpp
+++ conver.cpp
@@ -2,15 +2,17 @@
 //to convert decimal to binary 
 int n;
 void convert(int x){
-    if(x==0)
-    return;
+    if(x==0){
+        printf("%d",0);
+        return;
+    }
+    if(x==1){
+        return;
+    }
     convert(x/2);
     printf("%d",x%2);
 }
 int main(){
     scanf("%d",&n);
-    if(n!=0)
     convert(n);
-    else
-    printf("%d\n",0);
 }
