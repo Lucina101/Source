16965664b0ae40860218aca26a687fd97311c54e
Manza21 <supakornmanza21@gmail.com>
2018-08-30 14:52:18 +0700

    Add files via upload

---
+++ partition.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<algorithm>
+using namespace std;
+int a[100],n,ans;
+void genpartition(int x,int k){
+if(x==0){//if(check(a,k)==0){
+    for(int i=1;i<=k;i++){
+    printf("%d ",a[i]);
+    }
+    ans++;
+    printf("\n");//}
+return;
+}
+if(k==0) a[0]=x;
+for(int j=min(a[k],x);j>=1;j--){
+        a[k+1]=j;
+        genpartition(x-j,k+1);
+}
+}
+int main(){
+scanf("%d",&n);
+genpartition(n,0);
+printf("%d",ans);
+}
