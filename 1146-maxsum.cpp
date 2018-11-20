21d49ae74cc2a01c300378cad0b0727c7884c5bb
Manza21 <supakornmanza21@gmail.com>
2018-11-20 09:27:29 +0700

    Add files via upload

---
+++ 1146-maxsum.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,b[200][200],ans,x;
+int main(){
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            scanf("%d",&x);
+            b[i][j]=b[i-1][j]+b[i][j-1]-b[i-1][j-1];
+            b[i][j]+=x;
+        }
+    }
+    for(int i=1;i<=n;i++)
+    for(int j=1;j<=n;j++){
+        for(int k=i;k<=n;k++){
+            for(int l=j;l<=n;l++){
+                ans=max(ans,b[k][l]-b[i-1][l]-b[k][j-1]+b[i-1][j-1]);
+            }
+        }
+    }
+    printf("%d\n",ans);
+
+}
+/*
+0	−2	−7	0
+9	2	−6	2
+−4	1	−4	1
+−1	8	0	−2
+
+*/
