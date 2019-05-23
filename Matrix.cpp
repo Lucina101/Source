ba02698050e0eb29d5e1c52939146dffeaf8aff5
Manza21 <supakornmanza21@gmail.com>
2019-05-23 14:37:21 +0700

    Add files via upload

---
+++ Matrix.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int mod=1e9+7,Lucina=100;
+int a[105],n,k,s,t;
+inline int add(int a, int b) {
+  a+=b;return a>=mod?a-mod:a;
+}
+
+inline int sub(int a, int b) {
+  a -= b;return a<0?a+mod:a;
+}
+
+inline int mul(int a, int b) {
+  return (int) ((long long) a * b % mod);
+}
+
+inline int power(int a,int b) {
+  int res = 1;
+  while (b > 0) {
+    if (b & 1) {
+      res = mul(res, a);
+    }
+    a = mul(a, a);
+    b >>= 1;
+  }
+  return res;
+}
+struct matrix{
+    int m[105][105];
+    matrix (){
+        memset(m,0,sizeof(m));
+    }
+    matrix I(){
+        matrix tmp;
+        for(int i=0;i<=Lucina;i++)
+            tmp.m[i][i]=1;
+        return tmp;
+    }
+    matrix operator +(const matrix x)const{
+        matrix tmp;
+        for(int i=0;i<=Lucina;i++)
+            for(int j=0;j<=Lucina;j++)
+            tmp.m[i][j]=add(m[i][j],x.m[i][j]);
+        return tmp;
+    }
+    matrix operator *(const matrix x)const{
+        matrix tmp;
+        for(int i=0;i<=Lucina;i++)
+        for(int j=0;j<=Lucina;j++){
+            for(int r=0;r<=Lucina;r++){
+                tmp.m[i][j]=add(tmp.m[i][j],mul(m[i][r],x.m[r][j]));
+            }
+        }
+        return tmp;
+    }
+    matrix power(matrix x,int b){
+        matrix tmp=I();
+        while(b>0){
+            if(b&1){
+            tmp=(tmp*x);
+            }
+        x=(x*x);
+        b>>=1;
+        }
+        return tmp;
+    }
+    void print(){
+        for(int i=0;i<=Lucina;i++)
+            for(int j=0;j<=Lucina;j++)
+            printf("%d%c",m[i][j],j==Lucina?'\n':' ');
+    }
+};
+
+int main(){
+
+
+}
