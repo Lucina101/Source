394df3f8ad6ee0b25647f872906515118f7b16e6
Manza21 <supakornmanza21@gmail.com>
2019-12-12 20:16:59 +0700

    Update trash.cpp

--- trash.cpp
+++ trash.cpp
@@ -1,25 +1,41 @@
-int generator(int p) {
-    vector<int> fact;
-    int phi = p-1, n = phi;
-    for (int i = 2; i * i <= n; ++i) {
-        if (n % i == 0) {
-            fact.push_back(i);
-            while (n % i == 0)
-                n /= i;
-        }
-    }
-    if (n > 1)
-        fact.push_back(n);
 
-    for (int res = 2; res <= p; ++res) {
-        bool ok = true;
-        for (int factor : fact) {
-            if (powmod(res, phi / factor, p) == 1) {
-                ok = false;
-                break;
-            }
-        }
-        if (ok) return res;
+///segment tree to find kth number of tree ; sample implementation
+struct node{
+    int l , r , sum; // left id , right id , real sum
+    node(): l(0), r(0), sum(){} 
+}f[nax * 2 * Log]; 
+ 
+void build(int x, int l, int r){
+    f[x].sum = 0;
+    if(l == r) return ;
+    int mid = l + r >> 1;
+    f[x].l = ++cnt; build(f[x].l, l, mid);
+    f[x].r = ++cnt; build(f[x].r, mid + 1, r);
+}
+ 
+void update(int &x, int l, int r, int k){
+    f[++ cnt] = f[x]; ///cnt is global id start with 0
+    ++f[cnt].sum ; x = cnt;
+    if(l == r) return ;
+    int mid = l + r >> 1 ;
+    if(k <= mid) update(f[x].l, l, mid, k);
+    else update(f[x].r, mid + 1, r , k);
+}
+ 
+int kth(int x, int y, int v, int l,int r, int k){
+    if(l == r) return l;
+    int res = f[f[x].l].sum + f[f[y].l].sum - 2 * f[f[v].l].sum;
+    int mid = l + r >> 1;
+    if(res >= k) return kth(f[x].l, f[y].l, f[v].l, l, mid, k);
+    else return kth(f[x].r, f[y].r , f[v].r, mid + 1 , r, k - res);
+}
+
+void dfs(int nod, int pa, int cost){
+    if(pa > 0){
+        update(root[nod] = root[pa], 1, 100000, cost);
     }
-    return -1;
+    for(auto i: a[nod]){
+        if(i.first == pa) continue;
+        dfs(i.first, nod, i.second);
+    }
 }
