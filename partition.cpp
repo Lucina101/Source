e86483dd8af5649b187de095f968a8c29be69d74
Manza21 <supakornmanza21@gmail.com>
2020-03-23 20:46:16 +0700

    Update partition.cpp

--- partition.cpp
+++ partition.cpp
@@ -1,3 +1,4 @@
+/*
 #include<stdio.h>
 #include<algorithm>
 using namespace std;
@@ -22,4 +23,30 @@ int main(){
 scanf("%d",&n);
 partition(n,0);
 printf("%d",ans);
+}
+whoops compilation error code
+*/
+
+#include<cstdio>
+
+int b[32];
+
+void gen_par (int cur, int tar, int p ) {
+    if (cur == tar) {
+        for (int i = 1 ; i < p ; ++ i)
+            printf("%d ", b[i]);
+        printf("\n");
+        return ;
+    }
+    
+    for (int i = 1 ; i + cur <= tar ; ++ i) {
+        b[p] = cur + i;
+        gen_par(cur + i, tar, p + 1);
+    }
+}
+
+int main () {
+    
+    gen_par(0, 5, 1);
+
 }
